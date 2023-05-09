bacad9bd968144253d1990af320e01cd831f2a08
H4CK3R5 <h4ck3r5pc@gmail.com>
2023-05-09 09:09:03 +0200

    push

--- main.go
+++ main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"log"
 	"os"
 	"os/exec"
 	"regexp"
 	"strings"
+	"time"
 
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/pcap"
 	"github.com/sendgrid/sendgrid-go"
 	"github.com/sendgrid/sendgrid-go/helpers/mail"
 )
@@ -20,6 +24,12 @@ type EmailData struct {
 	browser      string
 	warninglevel int
 }
+
+var filter = flag.String("filter", "", "BPF filter for capture")
+var iface = flag.String("iface", "en0", "Select interface where to capture")
+var snaplen = flag.Int("snaplen", 1024, "Maximun sise to read for each packet")
+var promisc = flag.Bool("promisc", false, "Enable promiscuous mode")
+var timeoutT = flag.Int("timeout", 30, "Connection Timeout in seconds")
 
 func isSuspectLine(line string, ip string) bool {
 
@@ -79,12 +89,42 @@ func sendEmail() {
 		fmt.Println(response.Headers)
 	}
 }
+func sniffer() {
+	log.Println("start")
+	defer log.Println("end")
+
+	flag.Parse()
+
+	var timeout time.Duration = time.Duration(*timeoutT) * time.Second
+
+	handle, err := pcap.OpenLive(*iface, int32(*snaplen), *promisc, timeout)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer handle.Close()
+
+	if *filter != "" {
+		log.Println("applying filter ", *filter)
+		err := handle.SetBPFFilter(*filter)
+		if err != nil {
+			log.Fatalf("error applyign BPF Filter %s - %v", *filter, err)
+		}
+	}
+
+	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+
+	for packet := range packetSource.Packets() {
+		fmt.Println(packet)
+	}
+}
 
 func main() {
 
 	exec.Command("clear")
 	//accessLog est notre variable qui contient le fichier log et err sera la variable d'erreur
 	accessLog, err := os.Open("./var/log/apache2/access.log")
+	sniffer()
 
 	//si l'erreur n'est pas null alors on print l'erreur
 	if err != nil {
