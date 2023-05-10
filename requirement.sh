21ce9a6a2cc95abf8f6c3b04f95e2ad2df5649e9
H4CK3RS <75364799+H4CK3R5-Dmaster@users.noreply.github.com>
2023-05-10 10:13:28 +0200

    Update requirement.sh

--- requirement.sh
+++ requirement.sh
@@ -7,7 +7,29 @@ then
     sudo apt-get update
     sudo apt-get install -y golang-go
     echo "Golang a été installé avec succès."
-else
-    
-    echo "Golang est déjà installé sur votre système."
 fi
+
+mkdir /etc/IDS
+cp main.go /etc/IDS
+
+go run /etc/IDS/main.go
+
+
+sudo tee /etc/systemd/system/IDSmain.service <<EOF
+[Unit]
+Description=IDSmain service
+
+[Service]
+Type=simple
+Restart=always
+RestartSec=5s
+ExecStart=/usr/bin/go run /etc/IDS/main.go
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+
+sudo systemctl daemon-reload
+sudo systemctl enable IDSmain.service
+sudo systemctl start IDSmain.service
